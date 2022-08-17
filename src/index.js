39cdc3c0f685b1482c42b0d67a55c4090e616a8f
Freddy Florial <freddyflorial@gmail.com>
2022-08-17 18:33:24 -0400

    commit tree e9dab95e4351e9000097d3b2c0ed140e3633aee0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+// 1. add an event listener to the form element.
+  const inputForm = doucment.querySelector('form');
+// 2. Event Listeners require two arguments: the type of event, a string, and the listener, a callback function,    
+  inputForm.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    const input = document.querySelector('input#searchByID');
+    console.log(input.value);
+
+    fetch('http://localhost:3000/movies/${input.value}')
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = doucment.querySelector('selection#movieDetails p')
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+  })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
